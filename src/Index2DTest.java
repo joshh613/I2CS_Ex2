860cc3c5d5c46c123f0dd2dcb63bc83222cb9678
Josh Hall <github@joshuajacobhall.com>
2025-12-23 22:43:20 +0200

    scaffold tests

---
+++ src/Index2DTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Index2DTest {
+
+    @Test
+    void testConstructors() {
+    }
+
+    @Test
+    void getX() {
+    }
+
+    @Test
+    void getY() {
+    }
+
+    @Test
+    void distance2D() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
