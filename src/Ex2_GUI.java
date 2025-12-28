e25352069f973c751609df2cfa90d6c7f9108a5e
Josh Hall <github@joshuajacobhall.com>
2025-12-28 22:48:41 +0200

    started gui

--- src/Ex2_GUI.java
+++ src/Ex2_GUI.java
@@ -1,3 +1,8 @@
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Intro2CS_2026A
  * This class represents a Graphical User Interface (GUI) for Map2D.
@@ -9,17 +14,67 @@
  */
 public class Ex2_GUI {
     public static void drawMap(Map2D map) {
-        //
+        if (map == null) {
+            System.err.println("Error: map is null");
+            return;
+        }
+
+        int width = map.getWidth();
+        int height = map.getHeight();
+
+        StdDraw.enableDoubleBuffering();
+        StdDraw.clear();
+
+        StdDraw.setXscale(-0.5, width - 0.5);
+        StdDraw.setYscale(-0.5, height - 0.5);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int v = map.getPixel(x, y);
+                StdDraw.setPenColor(colorOf(v));
+                StdDraw.filledSquare(x, y, 0.5);
+            }
+        }
+
+        StdDraw.show();
     }
 
     /**
      * @param mapFileName
      * @return
      */
-    public static Map2D loadMap(String mapFileName) {
-        Map2D ans = null;
+    public static Map2D loadMap(String mapFileName) throws FileNotFoundException {
+        try (Scanner in = new Scanner(new File(mapFileName))) {
+            if (!in.hasNextInt()) {
+                System.err.println("empty / no width");
+                return null;
+            }
+            int w = in.nextInt();
 
-        return ans;
+            if (!in.hasNextInt()) {
+                System.err.println("no height");
+                return null;
+
+            }
+            int h = in.nextInt();
+
+            if (w <= 0 || h <= 0) {
+                System.err.println("invalid width or height");
+                return null;
+            }
+
+            int[][] map = new int[w][h];
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    if (!in.hasNextInt()) {
+                        System.err.println("invalid map");
+                        return null;
+                    }
+                    map[i][j] = in.nextInt();
+                }
+            }
+
+            return new Map(map);
+        }
     }
 
     /**
@@ -37,5 +92,19 @@ public class Ex2_GUI {
         Map2D map = loadMap(mapFile);
         drawMap(map);
     }
+
     /// ///////////// Private functions ///////////////
+
+    private static Color colorOf(int v) {
+        if (v == 0) {
+            return Color.WHITE;
+        } else if (v == 1) {
+            return Color.BLACK;
+        } else if (v == 2) {
+            return Color.BLUE;
+        } else {
+            // generic gray for any other value
+            return Color.LIGHT_GRAY;
+        }
+    }
 }
